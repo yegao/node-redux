5adc59ff3b609da52937c78dea550dd8c87c4dca
yegao <familyink@163.com>
2018-11-13 23:35:13 +0800

    dispatch & subscribe

--- index.js
+++ index.js
@@ -5,17 +5,36 @@ exports.splitPath = function (path) {
 };
 exports.createStore = function (reducer) {
     var state;
+    var list;
+    var listeners;
     var dispatch = function (action) {
+        list.push(action); //一般放到session中
         state = reducer(state, action);
+        for (var _i = 0, listeners_1 = listeners; _i < listeners_1.length; _i++) {
+            var listener = listeners_1[_i];
+            listener();
+        }
     };
     var getState = function () {
         return state;
     };
-    var interatePath = function (path) {
-        return exports.splitPath(path).reduce(function (prev, next) {
-            dispatch({ type: next });
-            return state;
-        });
+    var subscribe = function (listener) {
+        listeners.push(listener);
     };
-    return { dispatch: dispatch, getState: getState, interatePath: interatePath };
+    var hitAction = function (path, map, error) {
+        var paths = exports.splitPath(path);
+        var action = map;
+        for (var _i = 0, paths_1 = paths; _i < paths_1.length; _i++) {
+            var part = paths_1[_i];
+            if (!action[part]) {
+                return error("path not found");
+            }
+            action = action[part];
+        }
+        if (!action.type) {
+            return error("action has no type");
+        }
+        dispatch(action);
+    };
+    return { subscribe: subscribe, dispatch: dispatch, getState: getState, hitAction: hitAction };
 };
--- index.ts
+++ index.ts
@@ -6,10 +6,14 @@ type Reducer = (state:any,action:Action)=>{
 
 }
 
+type Listener = ()=>{
+
+}
 interface Store {
+  subscribe(listener:Listener),
   dispatch(action:Action),
   getState(),
-  interatePath(path:string)
+  hitAction(path:string,map:object,error:(arg:any)=>{})
 }
 
 export const splitPath = (path:string):string[]=>{
@@ -18,21 +22,39 @@ export const splitPath = (path:string):string[]=>{
 
 export const createStore = (reducer:Reducer):Store=>{
   let state:any;
+  let list:Action[];
+  let listeners:Listener[];
 
   const dispatch = (action:Action)=>{
-    state = reducer(state,action)
+    list.push(action);//一般放到session中
+    state = reducer(state,action);
+    for(let listener of listeners){
+      listener();
+    }
   }
 
   const getState = ():object=>{
     return state
   }
 
-  const interatePath = (path:string):any=>{
-    return splitPath(path).reduce((prev,next)=>{
-      dispatch({type:next})
-      return state;
-    })
+  const subscribe = (listener:Listener)=>{
+    listeners.push(listener)
   }
 
-  return { dispatch, getState ,interatePath };
+  const hitAction = (path:string,map:object,error:(arg:any)=>{}):any=>{
+    const paths = splitPath(path);
+    let action = map;
+    for(let part of paths){
+      if(!action[part]){
+        return error("path not found")
+      }
+      action = action[part];
+    }
+    if(!(<Action>action).type){
+      return error("action has no type")
+    }
+    dispatch(<Action>action)
+  }
+
+  return { subscribe, dispatch, getState ,hitAction };
 }
